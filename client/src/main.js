c3529a0a8a904d3a2fe7e376db32d9f033eebdf8
Ross Fletcher <rossfletcher108@gmail.com>
2025-05-22 14:49:26 +0100

    feat: information is sent to database

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,33 @@
+console.log("Week 4 assignment");
+
+// Selecting the form from the html form ID
+const commentsForm = document.getElementById("commentsForm");
+
 // TODO: Collect users data and send it to the server
 // - add interactivity to your data collection element (addEventListener to button)
+
+async function handleSubmitCommentsForm(event) {
+  event.preventDefault(); //Preventing defaults
+  console.log("Form submitted"); //Testing - confirm function runs
+  const formData = new FormData(commentsForm);
+  const name = formData.get("name");
+  const information = formData.get("information");
+  const comments = formData.get("comments"); // getting the value from the form field that matches with the attribute
+  const formValues = Object.fromEntries(formData); //Convering form data into plain JS
+
+  fetch("http://localhost:8080/comments", {
+    //Sending a request to the /comments section of my server
+    method: "POST", //Sending a post request
+    headers: {
+      "Content-Type": "application/json", // This tells the server we're sending stringified JSON data
+    },
+    body: JSON.stringify(formValues), //Sending the data as a string
+  });
+  console.log(formValues); //testing
+}
+
+commentsForm.addEventListener("submit", handleSubmitCommentsForm); //Calling the function when the comments form is submitted.
+
 // - make sure your event handler fetches the server route that creates new data in the database (same route as severjs?)
 // fetch ("url",{
 // method: "POST",
--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 // imports
+import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+import cors from "cors";
 
 // configs
 
+const app = express();
+app.use(express.json()); //Allows server to understand json data
+// by adding cors to our server, we allow resources (data) to reach our server to be processed, even if they come from a different origin
+app.use(cors());
+dotenv.config();
+
 // database pool
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 // port set up
 
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+
 // root route
+
+app.get("/", (request, response) => {
+  response.json({ message: "Welcome to my server. This is the root route!" });
+});
 
 // =============================================
 // TODO: A route to READ data from the databse
+
+app.get("/comments", (request, response) => {
+  response.json({ message: "This is the comments response" });
+});
+
+app.post("/comments", express.json(), async (req, res) => {
+  const { name, information, comments } = req.body;
+  console.log(req.body);
+  const query = await db.query(
+    `INSERT INTO comments (name, information, comments) VALUES ($1, $2, $3)`,
+    [name, information, comments]
+  );
+  res.json(query);
+});
 
 // TODO: A route to CREATE data in the database

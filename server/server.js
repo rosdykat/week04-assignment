5c412b51f776e38439d8fc2aa46542d65fa0665e
Ross Fletcher <rossfletcher108@gmail.com>
2025-05-23 11:03:19 +0100

    feat: data appends to page

--- server/server.js
+++ server/server.js
@@ -24,27 +24,45 @@ app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
 
-// root route
-
 app.get("/", (request, response) => {
   response.json({ message: "Welcome to my server. This is the root route!" });
+});
+
+// root route
+
+app.post("/comments", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const query = db.query(
+    `INSERT INTO comments (name, information, comments) VALUES ($1, $2, $3)`,
+    [body.name, body.information, body.comments]
+  );
+  res.json(query);
 });
 
 // =============================================
 // TODO: A route to READ data from the databse
 
-app.get("/comments", (request, response) => {
-  response.json({ message: "This is the comments response" });
+app.get("/comments", async (request, response) => {
+  // ! THIS WAS AN ERROR. Remember you cannot run two response.jsons in a .get
+  // response.json({ message: "This is the comments response" });
+  const result = await db.query(`SELECT * FROM comments`); // Selecting all rows in the databse
+  // I think I need to filter data to return the ID query string, to be able to delete it??
+  let data = query.rows;
+  if (queryString) {
+    data = data.filter((item) => {
+      return item.comments.id === queryString;
+    });
+  }
+
+  response.json(result.rows); //parsing the rows to the client as JSON
 });
 
-app.post("/comments", express.json(), async (req, res) => {
-  const { name, information, comments } = req.body;
-  console.log(req.body);
-  const query = await db.query(
-    `INSERT INTO comments (name, information, comments) VALUES ($1, $2, $3)`,
-    [name, information, comments]
-  );
-  res.json(query);
-});
+// This is handling posts requests, inserting the data into the comments table in my database (using the SQL I wrote in supabase)
+// This then sends the result back as a JSON response
 
-// TODO: A route to CREATE data in the database
+// ====== DELETE
+
+// app.get("/comments", function (req,res){
+
+// })

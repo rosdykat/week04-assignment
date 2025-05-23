3c12113039eed96197aa2791dbe0216de8cbc9a6
Ross Fletcher <rossfletcher108@gmail.com>
2025-05-23 11:02:55 +0100

    feat: data from database is appended to page

--- client/src/main.js
+++ client/src/main.js
@@ -28,13 +28,63 @@ async function handleSubmitCommentsForm(event) {
 
 commentsForm.addEventListener("submit", handleSubmitCommentsForm); //Calling the function when the comments form is submitted.
 
-// - make sure your event handler fetches the server route that creates new data in the database (same route as severjs?)
-// fetch ("url",{
-// method: "POST",
-// headers:{
-// },
-// body:
-// });
+// ========= Get comments data and display it on the page
+
+async function getComments() {
+  const response = await fetch("http://localhost:8080/comments");
+  const comments = await response.json();
+  console.log(comments);
+  return comments;
+}
+
+function createCommentsElements(arrayofdata) {
+  arrayofdata.forEach((item) => {
+    // Create the new elements
+    const formName = document.createElement("h2");
+    const formInformation = document.createElement("p");
+    const formComments = document.createElement("p");
+    const deleteButton = document.createElement("button");
+
+    // update content values
+    formName.textContent = item.name;
+    formInformation.textContent = item.information;
+    formComments.textContent = item.comments;
+    // I've created a delete button for each item, with this i'm hoping I can create an event listener to remove the select data row from the SQL database
+    deleteButton.textContent = "delete";
+    deleteButton.id = "delete" + item.id;
+
+    // Now create an event listener for the delete button?
+
+    // deleteButton.addEventListener("click", )
+
+    // append to DOM
+    const commentSection = document.getElementById("container");
+    commentSection.appendChild(formName);
+    commentSection.appendChild(formInformation);
+    commentSection.appendChild(formComments);
+    commentSection.appendChild(deleteButton);
+  });
+}
+
+async function renderData() {
+  const formData = await getComments();
+  console.log(formData);
+  createCommentsElements(formData);
+}
+
+// const deleteComment = document.getElementById("delete");
+// deleteComment.addEventListener("click", function (){
+// deleteComment.id
+
+// })
+
+renderData();
+
+// renderData();
+
+// =============================== I current render the data to the page. So I think my next step is managing this, deleting it, ect.
+
+// DELETING =====================================
 
 // ! - You will be fetching your local host during development. When your project is finished and deployed, make sure you swap your local host URLS with the server deployment URL.
 // Example: https://localhost:8080/staff --> development

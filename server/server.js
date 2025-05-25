35cc6fca76c68f7d09011724286be791af02461e
Ross Fletcher <rossfletcher108@gmail.com>
2025-05-25 15:18:58 +0100

    render

--- server/server.js
+++ server/server.js
@@ -20,14 +20,9 @@ const db = new pg.Pool({
 
 // port set up
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
-
-// app.listen(8080, () => {
-//   console.log("Server is running on port 8080");
-// });
 
 // root route
 
